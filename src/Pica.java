5dc938e7c10d41099abdb7e53a78b4f6c3eadb1e
Skrubis <ainarspro06@gmail.com>
2025-04-02 10:14:27 +0300

    Konstruktora izveide

--- src/Pica.java
+++ src/Pica.java
@@ -1,8 +1,14 @@
 
 public class Pica {
-
+	String toping,style;
+	int size;
+	public Pica(String toping, String style, int size) {
+		this.toping = toping;
+		this.style=style;
+		this.size=size;
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
