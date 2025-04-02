e924a3293d87b95a1826cd59cbded1a543cc782a
Skrubis <ainarspro06@gmail.com>
2025-04-02 10:27:29 +0300

    Izveidots sakuma logs

--- src/Pica.java
+++ src/Pica.java
@@ -1,3 +1,9 @@
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
 
 public class Pica {
 	String toping,style;
@@ -8,8 +14,30 @@ public class Pica {
 		this.size=size;
 	}
 	public static void main(String[] args) {
-		
-
+		JFrame frame = new JFrame("Pica menu");
+        frame.setSize(300, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new FlowLayout());
+        
+        JButton klientButton = new JButton("Pieņemt klientu");
+        JButton picaButton = new JButton("Taisīt picu");
+        
+        klientButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            }
+        });
+        
+        picaButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            }
+        });
+        
+        frame.add(klientButton);
+        frame.add(picaButton);
+        
+        frame.setVisible(true);
 	}
 
 }
