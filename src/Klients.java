f44c948b73913811e20156d160f4343fc33dc1ca
Skrubis <ainarspro06@gmail.com>
2025-04-03 08:19:36 +0300

    Izveidots konstruktors klients

--- src/Klients.java
+++ src/Klients.java
@@ -1,4 +1,12 @@
 
 public class Klients {
-
+	String style, toping;
+	int size,veids;
+	public Klients(int veids,String style,String toping,int size) {
+		this.veids=veids;
+		this.toping = toping;
+        this.style = style;
+        this.size = size;
+	}
+	
 }
